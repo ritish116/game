312cc1fc99b860b18bc7717e902f103d7aca5470
ritish116 <145608445+ritish116@users.noreply.github.com>
2024-08-25 01:58:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes){
+    const index  = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+
+    
+    if (hole === lastHole){
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+
+function peep() {
+    const time = randomTime(1000, 1000); 
+    const hole = randomHole(holes); 
+    hole.classList.add('up'); 
+    setTimeout(() => {
+        hole.classList.remove('up'); 
+        if(!timeUp) {
+            peep();
+        }
+    }, time);
+}
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 30000) //show random moles for 15 seconds
+}
+
+function wack(e){
+    if(!e.isTrusted) return; //** new thing I learned */
+    score++;
+    this.parentNode.classList.remove('up'); //this refers to item clicked
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', wack))
+
